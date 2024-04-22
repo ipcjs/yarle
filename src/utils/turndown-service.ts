423ef44f614e4d80b5140065e9409fd6838b1051
ipcjs <gipcjs@gmail.com>
2024-04-23 03:31:27 +0800

    fix: prevent `$` from being escaped

--- src/utils/turndown-service.ts
+++ src/utils/turndown-service.ts
@@ -78,6 +78,7 @@ export const getTurndownService = (yarleOptions: YarleOptions) => {
     } else {
         const escapes = [...defaultEscapes]
         if (yarleOptions.outputFormat === OutputFormat.ObsidianMD) {
+            escapes.push([/\$/g, '\\$$'])
             escapes.push([/</g, '\\<'])
             escapes.push([/(~{2,})/g, (sub) => sub.replace(/(~{1,2})/g, '\\$1')])
             escapes.push([/_+\b|\b_+/g, (sub) => sub.replace(/_/g, '\\_')])
