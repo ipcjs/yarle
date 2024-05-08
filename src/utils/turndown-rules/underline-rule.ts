436cda874aeab8418f43aa368bc6ec3f57626562
ipcjs <gipcjs@gmail.com>
2024-05-08 23:48:57 +0800

    fix: remove underline when it contain a link

--- src/utils/turndown-rules/underline-rule.ts
+++ src/utils/turndown-rules/underline-rule.ts
@@ -1,9 +1,16 @@
+import { OutputFormat } from '../../output-format';
+import { yarleOptions } from '../../yarle';
 
 // Note: this rule must appear *after* use(gfm) so it can override
 // turndown-plugin-gfm rule for strikethrough (which always uses single '~')
 export const underlineRule = {
   filter: ['u'],
-  replacement: (content: any) => {
-   return `<u>${content}</u>`
+  replacement: (content: any, node: HTMLElement, options: any) => {
+    if (yarleOptions.outputFormat === OutputFormat.ObsidianMD &&
+      node.childNodes.length === 1 && node.childNodes[0].nodeName === 'A') {
+      // In obsidian's preview mode, the underline displays abnormally when it contains a link, so the underline is removed.
+      return content
+    }
+    return `<u>${content}</u>`
   },
 };
